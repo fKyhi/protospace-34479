e5341f65e7762771476fa6f0e7becfc150c7d4e2
fKyhi <78200228+fKyhi@users.noreply.github.com>
2021-02-17 19:01:06 +0900

    Update 20210215063134_create_comments.rb

--- db/migrate/20210215063134_create_comments.rb
+++ db/migrate/20210215063134_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.references :user_id
-      t.references :prototype_id
+      t.references :user
+      t.references :prototype
       t.text :text
       t.timestamps
     end
